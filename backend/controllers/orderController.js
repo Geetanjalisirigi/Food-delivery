ab50b2f7ab510d9f9a20ae8e5d6e1c4a180f4451
geetanjali <geetanjalisirigibattula@gmail.com>
2024-11-11 17:30:27 +0530

    before payment

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,13 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js"
+import srtipe from "stripe"
+
+
+
+//placing user order for frontend
+const placeOrder=async(req,res)=>{
+
+}
+
+
+export {placeOrder}
